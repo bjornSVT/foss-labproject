4d063562f63c499947b0c8e6678692a1184d2216
Josef Andersson <josef.andersson@svt.se>
2021-02-03 11:06:44 +0100

    add axion conf
    Signed-off-by: Josef Andersson <josef.andersson@svt.se>

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,13 +2,13 @@ plugins {
     `maven-publish`
     idea
     kotlin("jvm") version "1.3.72"
-    id("pl.allegro.tech.build.axion-release") version "1.11.0"
+    id("pl.allegro.tech.build.axion-release") version "1.12.1"
     id("com.github.ben-manes.versions") version "0.28.0"
     id("com.jfrog.bintray") version "1.8.4"
 }
 
 apply {
-    from("initial-version.gradle")
+    from("axion-release-config.gradle")
 }
 
 group = "se.svt.oss"
